6374171a8e31eb130b98a9d8a714a3b38935bc82
mac <lixiang@lerjin.onaliyun.com>
2021-10-16 11:52:55 +0800

    1.UI优化，私聊状态下的语音UI展示修改

--- im-cc-ui/src/main/java/com/zj/imUi/widget/GroupMessageRecordItem.kt
+++ im-cc-ui/src/main/java/com/zj/imUi/widget/GroupMessageRecordItem.kt
@@ -60,14 +60,20 @@ open class GroupMessageRecordItem @JvmOverloads constructor(context: Context, at
                 audioPlayView.setPaintColor(ContextCompat.getColor(context,R.color.im_msg_text_color_white))
             }else if (data.getReplyMsgType() == UiMsgType.MSG_TYPE_QUESTION ) {
                 if (data.getSenderId() == data.getSelfUserId()){//大v自己发送打赏录音消息
-                    if (data.getReplyMsgQuestionIsPublished() == false&&chatType == 1){
+                    if (data.getReplyMsgQuestionIsPublished() == false && chatType == 1){
                         audioLinearLayout.setBackgroundResource(R.drawable.im_msg_item_audio_white_cornors_bg)
                         audioTime.setTextColor(ContextCompat.getColor(context, R.color.im_msg_bg_purple))
                         audioPlayView.setPaintColor(ContextCompat.getColor(context, R.color.im_msg_bg_purple))
                     }else{
-                        audioLinearLayout.setBackgroundResource(R.drawable.im_msg_item_audio_white_cornors_bg)
-                        audioTime.setTextColor(ContextCompat.getColor(context, R.color.im_msg_bg_origin))
-                        audioPlayView.setPaintColor(ContextCompat.getColor(context, R.color.im_msg_bg_origin))
+                        if (chatType == UiMsgType.PRIVATE_CHAT){
+                            audioLinearLayout.setBackgroundResource(R.drawable.im_msg_item_audio_self_origin_cornors_bg)
+                            audioTime.setTextColor(ContextCompat.getColor(context, R.color.im_msg_text_color_white))
+                            audioPlayView.setPaintColor(ContextCompat.getColor(context,R.color.im_msg_text_color_white))
+                        }else {
+                            audioLinearLayout.setBackgroundResource(R.drawable.im_msg_item_audio_white_cornors_bg)
+                            audioTime.setTextColor(ContextCompat.getColor(context, R.color.im_msg_bg_origin))
+                            audioPlayView.setPaintColor(ContextCompat.getColor(context, R.color.im_msg_bg_origin))
+                        }
                     }
                 }else{
                     if (data.getReplyMsgQuestionIsPublished() == false&&chatType == 1)
